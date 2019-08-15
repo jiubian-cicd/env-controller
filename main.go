8a1062e26988dcb5cfd801ffa13f36dab7a23bc0
jiangyu.zjy <jiangyu.zjy@alibaba-inc.com>
2019-08-15 17:15:17 +0800

    add env controller cmd

--- main.go
+++ main.go
@@ -1,26 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+	"github.com/jiubian-cicd/env-controller/pkg/cmd"
+	"os"
+	"github.com/jenkins-x/jx/pkg/cmd/clients"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example"
 
-	from := ""
-	if r.URL != nil {
-		from = r.URL.String()
+// Run runs the command, if args are not nil they will be set on the command
+func Run(args []string) error {
+	cmd := cmd.NewJXCommand(clients.NewFactory(), os.Stdin, os.Stdout, os.Stderr, nil)
+	if args != nil {
+		args = args[1:]
+		cmd.SetArgs(args)
 	}
-	if from != "/favicon.ico" {
-		log.Printf("title: %s\n", title)
-	}
-
-	fmt.Fprintf(w, "Hello from:  "+title+"\n")
-}
-
-func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	return cmd.Execute()
 }
